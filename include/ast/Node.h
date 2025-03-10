f34c46577ac7cd4889f816158f878ba0fc8b281b
Mattias Ajander <mattias@ajander.se>
2025-03-10 07:59:35 +0100

    Added NodeValue to for later implementation (possibly)

--- include/ast/Node.h
+++ include/ast/Node.h
@@ -5,6 +5,10 @@
 namespace funk
 {
 
+// TODO: This is a idea to simplify node values, implementing a generic literal type
+// not separate it in different classes.
+using NodeValue = std::variant<std::monostate, int, double, bool, char, String, Node*>;
+
 class Node
 {
 public:
